fd0bca2e94bc2c064e6bb8a7d4ce68df85c14146
Justin Cook <justin.cook.dev@gmail.com>
2018-11-05 10:55:51 -0800

    Some tests.

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -3,7 +3,9 @@ describe('Bubble Sort', function(){
     expect( bubbleSort([]) ).toEqual( [] );
   });
   it('handles an array of two elements', function(){
-    expect( bubbleSort([2,1]) ).toEqual( [1,2] )
+    expect( bubbleSort([2,1]) ).toEqual( [1,2] );
+    expect( bubbleSort([3,4])).toEqual( [3,4] );
+    expect( bubbleSort(['a', 'A', 'c', 'B'])).toEqual( ['A', 'B', 'a', 'c']);
   });
   it('handles an array of multiple elements', function(){
     ex

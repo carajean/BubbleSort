13fb1aca6daae3cee5693a075da4e63f863e0ae1
Carolyn <ccpianist@gmail.com>
2018-11-05 11:11:20 -0800

    spyOn and initial bubbleSort fx

---
+++ bubblesort.js
@@ -0,0 +1,11 @@
+function bubbleSort(arr) {
+    let sorted = [];
+    for (let i = 0; i < arr.length; i++){
+        //sort
+    }
+    return sorted;
+}
+
+bubbleSort.swap = () => {
+
+}
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -3,13 +3,20 @@ describe('Bubble Sort', function(){
     expect( bubbleSort([]) ).toEqual( [] );
   });
   it('handles an array of two elements', function(){
-    expect( bubbleSort([2,1]) ).toEqual( [1,2] );
-    expect( bubbleSort([3,4])).toEqual( [3,4] );
-    expect( bubbleSort(['a', 'A', 'c', 'B'])).toEqual( ['A', 'B', 'a', 'c']);
+    expect( bubbleSort( [2,1]) ).toEqual( [1,2] );
+    expect( bubbleSort( [3,4])).toEqual( [3,4] );
+    expect( bubbleSort( ['a', 'B'])).toEqual( ['B', 'a'] );
   });
   it('handles an array of multiple elements', function(){
-    ex
+    expect( bubbleSort( [123, 58, 226])).toEqual( [58, 123, 226] );
+    expect( bubbleSort( [9, 'apple', 'Zebra'])).toEqual( [9, 'Zebra', 'apple'] );
+    expect( bubbleSort( ['a', 'A', 'c', 'B'])).toEqual( ['A', 'B', 'a', 'c'] );
   })
+
+  beforeAll(function () {
+    spyOn('bubbleSort', 'swap').and.callThrough();
+    
+  }
 });
 
 
